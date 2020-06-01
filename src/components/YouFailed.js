15acf89aae06cab5ce4218594262bf05e89b0edc
niyas <hojaniyazhan@yahoo.com>
2020-06-01 05:06:42 +0200

    you failed image added

---
+++ src/components/YouFailed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+
+const YouFailed = () => (
+  <Image
+    src="https://www.minecraftforge.net/forum/uploads/monthly_2019_12/1618190649_minefuckyou.png.9439d6525eda4ee7ccab90aa85738e81.png"
+    fluid
+  />
+);
+
+export default YouFailed;
--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Form, Button, Input, Grid, Table, Modal } from 'semantic-ui-react';
+import YouFailed from '../components/YouFailed';
 import Data from '../db.json';
 
 let RANDOM_COUNT = 5;
 
 const DefaultPage = () => {
+  const [modal, setModal] = useState(false);
+  const [randomQuestions, setRandomQuestions] = useState([]);
+  const [active, setActive] = useState(false);
+  const [unsuccessQuestions, setUnsuccessQuestions] = useState([]);
+  const [useRedirect, setUseRedirect] = useState(false);
+  const [blockApplication, setBlockApplication] = useState(false);
   const [givenAnswer, setGivenAnswer] = useState({
     question: {
       id: 0,
@@ -14,11 +21,6 @@ const DefaultPage = () => {
     },
     response: '',
   });
-  const [modal, setModal] = useState(false);
-  const [randomQuestions, setRandomQuestions] = useState([]);
-  const [active, setActive] = useState(false);
-  const [unsuccessQuestions, setUnsuccessQuestions] = useState([]);
-  const [useRedirect, setUseRedirect] = useState(false);
 
   const { Questions } = Data;
 
@@ -33,6 +35,7 @@ const DefaultPage = () => {
 
     if (unsuccessQuestions.length === 3) {
       setModal(true);
+      setBlockApplication(true);
       unsuccessQuestions.length = 0;
     }
   }, [Questions, unsuccessQuestions.length]);
@@ -62,75 +65,80 @@ const DefaultPage = () => {
 
   return (
     <>
-      {' '}
-      <Modal open={modal} dimmer="blurring">
-        <Modal.Header>{unsuccessQuestions.length ? 'ERROR' : 'YOU FAILED'}</Modal.Header>
-        <Modal.Content>
-          {unsuccessQuestions.length ? (
-            <p>Wrong Answer! {unsuccessQuestions.length}</p>
-          ) : (
-            <p>You gave 3 wrong answer. You couldn't pass.</p>
-          )}
-        </Modal.Content>
-        <Modal.Actions>
-          <Button onClick={() => setModal(false)} content="OK" />
-        </Modal.Actions>
-      </Modal>
-      {randomQuestions &&
-        randomQuestions.map((question) => (
-          <Grid className="segment centered">
-            <Button color="blue" key={question.id} onClick={() => onButtonClick(question)}>
-              {question.question}
-            </Button>
-            {active && (
-              <Form onSubmit={(event) => submitHandler(event)} size="big">
-                <Form.Group inline>
-                  <Form.Field>
-                    <Input
-                      placeholder="your answer"
-                      id={question.id}
-                      name="userAnswer"
-                      onChange={(event) =>
-                        setGivenAnswer({
-                          question,
-                          response: event.target.value,
-                        })
-                      }
-                    />
-                  </Form.Field>
-                  <Form.Field
-                    control={Button}
-                    color="green"
-                    icon="check"
-                    content="Submit"
-                    type="submit"
-                  />
-                </Form.Group>
-              </Form>
-            )}
-          </Grid>
-        ))}
-      <Table definition>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell />
-            <Table.HeaderCell>Unsuccess Question</Table.HeaderCell>
-            <Table.HeaderCell>Given Wrong Answer</Table.HeaderCell>
-            <Table.HeaderCell>Correct Answer</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {unsuccessQuestions &&
-            unsuccessQuestions.map((question, index) => (
-              <Table.Row>
-                <Table.Cell>{index + 1}</Table.Cell>
-                <Table.Cell>{question.question.question}</Table.Cell>
-                <Table.Cell>{question.response}</Table.Cell>
-                <Table.Cell>{question.question.answer}</Table.Cell>
-              </Table.Row>
+      {blockApplication ? (
+        <YouFailed />
+      ) : (
+        <>
+          <Modal open={modal} dimmer="blurring">
+            <Modal.Header>{unsuccessQuestions.length ? 'ERROR' : 'YOU FAILED'}</Modal.Header>
+            <Modal.Content>
+              {unsuccessQuestions.length ? (
+                <p>Wrong Answer! {unsuccessQuestions.length}</p>
+              ) : (
+                <p>You gave 3 wrong answer. You couldn't pass.</p>
+              )}
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={() => setModal(false)} content="OK" />
+            </Modal.Actions>
+          </Modal>
+          {randomQuestions &&
+            randomQuestions.map((question) => (
+              <Grid className="segment centered">
+                <Button color="blue" key={question.id} onClick={() => onButtonClick(question)}>
+                  {question.question}
+                </Button>
+                {active && (
+                  <Form onSubmit={(event) => submitHandler(event)} size="big">
+                    <Form.Group inline>
+                      <Form.Field>
+                        <Input
+                          placeholder="your answer"
+                          id={question.id}
+                          name="userAnswer"
+                          onChange={(event) =>
+                            setGivenAnswer({
+                              question,
+                              response: event.target.value,
+                            })
+                          }
+                        />
+                      </Form.Field>
+                      <Form.Field
+                        control={Button}
+                        color="green"
+                        icon="check"
+                        content="Submit"
+                        type="submit"
+                      />
+                    </Form.Group>
+                  </Form>
+                )}
+              </Grid>
             ))}
-        </Table.Body>
-      </Table>{' '}
+          <Table definition>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell />
+                <Table.HeaderCell>Unsuccess Question</Table.HeaderCell>
+                <Table.HeaderCell>Given Wrong Answer</Table.HeaderCell>
+                <Table.HeaderCell>Correct Answer</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {unsuccessQuestions &&
+                unsuccessQuestions.map((question, index) => (
+                  <Table.Row>
+                    <Table.Cell>{index + 1}</Table.Cell>
+                    <Table.Cell>{question.question.question}</Table.Cell>
+                    <Table.Cell>{question.response}</Table.Cell>
+                    <Table.Cell>{question.question.answer}</Table.Cell>
+                  </Table.Row>
+                ))}
+            </Table.Body>
+          </Table>{' '}
+        </>
+      )}{' '}
     </>
   );
 };
